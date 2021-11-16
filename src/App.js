e7099e4644bcf81a17277c557d977cb5e348029f
edlopez000 <edlopez000>
2021-11-16 15:24:52 -0500

    First day of class

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  ChakraProvider,
+  Input,
+  Text,
+  Box,
+  AspectRatio,
+} from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box ml="5em">
+        <Text>Users Sign Up Form</Text>
+        <Text>Name</Text>
+        <Input placeholder="Full Name" />
+        <Text>Password</Text>
+        <Input placeholder="password" />
+        <Text>Email</Text>
+        <Input placeholder="email" />
+        <AspectRatio maxW="560px" ratio={1}>
+          <iframe
+            title="naruto"
+            src="https://www.youtube.com/embed/QhBnZ6NPOY0"
+            allowFullScreen
+          />
+        </AspectRatio>
+      </Box>
+    </ChakraProvider>
   );
 }
 
